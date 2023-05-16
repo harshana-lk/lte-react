1e379cd532f61bad55bfebaef40fedd5777af28d
harshana-lk <harshanajayaweerayt@gmail.com>
2023-05-16 14:31:09 +0530

    ui changes

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -21,14 +21,14 @@ const SideNav = () => {
           <div className="user-panel mt-3 pb-3 mb-3 d-flex">
             <div className="image">
               <img
-                src="dist/img/user2-160x160.jpg"
+                src="dist/img/AI.png"
                 className="img-circle elevation-2"
                 alt="User Image"
               />
             </div>
             <div className="info">
               <a href="#" className="d-block">
-                Alexander Pierce
+                Harshana Jayaweera
               </a>
             </div>
           </div>
